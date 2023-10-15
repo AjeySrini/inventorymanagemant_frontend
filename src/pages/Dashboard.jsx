5c274a35933fd722fe018e88aaec355b3acf7584
Ajey <ajeysrini@gmail.com>
2023-10-15 20:36:38 +0530

    dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <h1>Dashboard page</h1><br></br>
-            <p>To Add Product to inventory -- <Link to="/product">Click here</Link></p>
-        </div>
-    );
+  return (
+    <div className="dashbord">
+      <h1>Dashboard </h1>
+      <br></br>
+      <div className="landingDash">
+        <h1> Inventory Management</h1><br></br>
+        <div >
+          <p>
+            Detailed of Product, Suppliers , quick stock reports, and accurate
+            inventory and Analysis of goods 
+          </p>
+          <ul>
+            <ol>
+              Keep a&nbsp;record of&nbsp;different product
+               variants and prices and its condition
+            </ol>
+            <ol>
+              Records of Sold goods and product 
+            </ol>
+            <ol>
+              Monitor stock level using detailed reports
+            </ol>
+          </ul>
+        </div><img src="/dashimg.png" alt="dashimg" width="300" height="300"/>
+      </div><br></br><br></br>
+      <p>
+        To Add new Product to inventory -- <Link to="/product">Click here</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Dashboard;
